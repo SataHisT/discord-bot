b7987453ecd7a81fbfb7e71e9cc7e1bcf1131e48
user <wolf21ru@yandex.ru>
2024-09-09 11:57:33 +0500

    bot-logger: new events & change deployed logic

--- logger-bot/deploy-commands.js
+++ logger-bot/deploy-commands.js
@@ -1,28 +1,40 @@
-const { REST, Routes } = require('discord.js');
-const fs = require('fs');
-const { clientId, guildId, token } = require('./config/config.json');
+const { REST, Routes } = require('discord.js')
+const fs = require('fs')
+require('dotenv').config()
 
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const token = process.env.BOT_TOKEN
+const clientId = process.env.BOT_CLIENT_ID
+const guildId = process.env.BOT_GUILD_ID
 
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
+if (!token || !clientId || !guildId) {
+	console.error('Отсутствуют переменные окружения. Убедитесь, что BOT_TOKEN, BOT_CLIENT_ID и BOT_GUILD_ID установлены.')
+	process.exit(1)
 }
 
-const rest = new REST({ version: '10' }).setToken(token);
+const commands = []
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'))
 
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`)
 
-        await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            { body: commands },
-        );
+	if (!command.data) {
+		console.error(`Команда ${file} не содержит свойства data.`)
+	} else {
+		console.log(`Команда ${file} успешно загружена.`)
+		commands.push(command.data.toJSON())
+	}
+}
 
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
-    }
-})();
+const rest = new REST({ version: '10' }).setToken(token)
+
+;(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.')
+
+		await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+
+		console.log('Successfully reloaded application (/) commands.')
+	} catch (error) {
+		console.error(error)
+	}
+})()
